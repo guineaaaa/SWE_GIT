896b38c63f04adfbf29cd115ba6978e4ddf3d37b
guineaaaa <165776804+guineaaaa@users.noreply.github.com>
2025-12-07 15:51:49 +0900

    master 브랜치에서의 C.java 수정

--- SWE_GIT/src/mygit/C.java
+++ SWE_GIT/src/mygit/C.java
@@ -10,7 +10,8 @@ public class C {
 		System.out.println("=== 성적 관리 프로그램 ===");
 		System.out.println("학생 :"+student.name);
 		System.out.println("점수: "+score.subjectScore);
-		System.out.println("결과: 데이터없음");
+		// Master 브랜치에서 총점 계산 출력문으로 수정 (Alice의 '평균' 출력문과 충돌)
+		System.out.println("결과: 총점 합계 계산중...");
 
 	}
 
