d652e8693e4a8a3526f364d0af97819bd6351aa4
guineaaaa <165776804+guineaaaa@users.noreply.github.com>
2025-12-07 15:53:06 +0900

    Bob 브랜치에서의 A.java 이름 수정, B.java의 성적 수정

--- SWE_GIT/src/mygit/A.java
+++ SWE_GIT/src/mygit/A.java
@@ -1,7 +1,7 @@
 package mygit;
 
 public class A {
-	// 학생 정보 클래스
-	public String name="미정";
+	// 학생 정보 클래스, Bob이 본인 이름으로 수정 (Alice 수정사항과 충돌)
+	public String name="Bob";
 
 }
--- SWE_GIT/src/mygit/B.java
+++ SWE_GIT/src/mygit/B.java
@@ -1,7 +1,7 @@
 package mygit;
 
 public class B {
-	// 점수 저장 클래스
-	public int subjectScore=0;
+	// 점수 저장 클래스, Bob의 수정, 충돌 없음
+	public int subjectScore=100;
 
 }
